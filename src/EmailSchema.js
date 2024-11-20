d3697dbeed206ec048151c876bf4fcfa007b36f3
Екимов Георгий <ekimovgosha@mail.ru>
2024-11-20 11:28:46 +0300

    task 1-2

--- src/EmailSchema.js
+++ src/EmailSchema.js
@@ -1,1 +1,27 @@
+class EmailValidator {
+  constructor() {
+    this.minLength = null;
+    this.maxLength = null;
+  }
 
+  isValid(input) {
+    if (typeof input !== 'string') return false;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(input.trim())) return false;
+
+    const localPart = input.split('@')[0];
+
+    if (this.minLength !== null && localPart.length < this.minLength) return false;
+    if (this.maxLength !== null && localPart.length > this.maxLength) return false;
+
+    return true;
+  }
+
+  setEmailLengthConstraint(min, max = null) {
+    this.minLength = min;
+    this.maxLength = max;
+    return this;
+  }
+}
+export default EmailValidator;
--- src/Validator.js
+++ src/Validator.js
@@ -1,1 +1,10 @@
+import EmailValidator from './EmailSchema.js';
 
+class Validator {
+  // Метод, создающий валидатор для чисел
+  email() {
+    return new EmailValidator();
+  }
+}
+
+export default Validator;
