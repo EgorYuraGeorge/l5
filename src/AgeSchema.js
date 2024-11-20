d4dadb017a193b441bf61986e6c7b3b37c12a28d
Екимов Георгий <ekimovgosha@mail.ru>
2024-11-20 12:12:54 +0300

    task

--- src/AgeSchema.js
+++ src/AgeSchema.js
@@ -1,1 +1,20 @@
-console.log('Hello, World');
+class AgeValidator {
+  constructor() {
+    this.checkAdult = false;
+  }
+
+  isValid(input) {
+    if (typeof input !== 'number' || Number.isNaN(input)) return false;
+
+    if (this.checkAdult && input < 18) return false;
+
+    return true;
+  }
+
+  isAdult() {
+    this.checkAdult = true;
+    return this;
+  }
+}
+
+export default AgeValidator;
--- src/Validator.js
+++ src/Validator.js
@@ -1,9 +1,14 @@
 import EmailValidator from './EmailSchema.js';
+import AgeValidator from './AgeSchema.js';
 
 class Validator {
   // Метод, создающий валидатор для чисел
   email() {
     return new EmailValidator();
+  }
+
+  age() {
+    return new AgeValidator();
   }
 }
 
